9029934533d6264d268c134bca43e4526e2772ac
jeffjb4488 <66730109+jeffjb4488@users.noreply.github.com>
2022-02-19 18:30:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
 
-$.getJSON("polygon_test_wgs.geojson", function (data) {
+$.getJSON("town-home-value-index.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
     style: style,
     onEachFeature: onEachFeature
